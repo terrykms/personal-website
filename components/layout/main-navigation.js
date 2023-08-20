dab42f2951427d3687e44ec0384ab9172ee8c41a
Kim Minseo <terrykms30@gmail.com>
2023-08-20 21:50:28 +0800

    initial commit

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import classes from "./main-navigation.module.scss";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <Link href="/">Home</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <h1>Hello!</h1>
+      <h2>Hello!</h2>
+    </Fragment>
+  );
+};
+
+export default HomePage;
