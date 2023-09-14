e33071ad0fb13c9958da551e4f9e2b7c83ac148d
Kim Minseo <terrykms30@gmail.com>
2023-09-14 22:35:46 +0800

    fixed google font imports & added next/font/google

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import Head from "next/head";
 import Layout from "@/components/layout/layout";
+import { Open_Sans, Oswald } from "next/font/google";
+
 import "@/styles/globals.scss";
+
+const openSans = Open_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+});
+
+const oswald = Oswald({
+  subsets: ["latin"],
+  variable: "--font-oswald",
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +21,12 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <style jsx global>{`
+        :root {
+          --font-open-sans: ${openSans.style.fontFamily};
+          --font-oswald: ${oswald.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </Layout>
   );
