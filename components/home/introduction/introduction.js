ac930cb4c3ae7b54acd7a9326646dd2c47f589f0
Kim Minseo <terrykms30@gmail.com>
2025-07-27 11:03:32 +0800

    edited introduction & new internship

--- components/home/introduction/introduction.js
+++ components/home/introduction/introduction.js
@@ -17,22 +17,26 @@ const Introduction = () => {
         <h1>Minseo Kim</h1>
         <p>Welcome to my page!</p>
         <p>
-          I am an undergraduate in{" "}
+          I'm a final-year undergraduate at{" "}
           <span className={classes.highlight}>
             Nanyang Technological University
           </span>
-          , currently taking a 2-year gap to complete my{" "}
-          <span className={classes.highlight}>National Service (NS)</span> in
-          Singapore.
+          , majoring in Chemical and Biomolecular Engineering.
         </p>
         <p>
-          While my degree is in Chemical Engineering, I managed to deepen my
-          knowledge in programming through{" "}
-          <Link href="/projects">personal projects</Link> and consolidating my
-          learnings through writing <Link href="/articles">articles</Link>{" "}
-          during my time in NS.
+          Beyond my core studies, I've developed a strong interest in software
+          development, which I've pursued through{" "}
+          <Link href="/projects">personal projects</Link> and{" "}
+          <Link href="/articles">technical writing</Link> to consolidate and
+          share my learning.
         </p>
-        <p>I will be back in August 2025 to complete my final year at NTU!</p>
+        <p>
+          Currently, I'm working at{" "}
+          <span className={classes.highlight}>U-Reg</span> as a{" "}
+          <span className={classes.highlight}>Software Engineering Intern</span>
+          , where I contribute to building AI-integrated features across the
+          platform.
+        </p>
       </div>
     </div>
   );
